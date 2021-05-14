d0183b3d87bb9a83bdbc9fbbbb122fc7182e4e6b
Salov Nikolay <elluminatte@gmail.com>
2021-05-14 14:52:54 +0300

    Refs #24460: API extenstions - removed unused custom-product extension; implemented budsies extension with printed products add to cart proxy
    Refs #24460: API extenstions - removed unused custom-product extension; implemented budsies extension with printed products add to cart proxy

---
+++ src/api/extensions/budsies/index.js
@@ -0,0 +1,43 @@
+import { apiStatus, getToken } from '../../../lib/util';
+import { Router } from 'express';
+import { multiStoreConfig } from '../../../platform/magento1/util';
+
+const Magento1Client = require('magento1-vsbridge-client').Magento1Client
+
+module.exports = ({ config, db }) => {
+  function getResponse(data){
+    if(data.code === 200){
+        return data.result;
+    }
+
+    return false;
+  }
+
+  let budsiesApi = Router();
+
+  budsiesApi.post('/printed-products/cart-items', (req, res) => {
+    const client = Magento1Client(multiStoreConfig(config.magento1.api, req));
+
+    client.addMethods('budsies', (restClient) => {
+      let module = {};
+
+      module.addPrintedProductToCart = function () {
+        const customerToken = getToken(req);
+
+        return restClient.post(`printedProduct/create?token=${customerToken}`, req.body).then((data) => {
+            return getResponse(data);
+        });
+      }
+
+      return module;
+    });
+
+    client.budsies.addPrintedProductToCart().then((result) => {
+      apiStatus(res, result, 200);
+    }).catch(err => {
+      apiStatus(res, err, 500);
+    });
+  })
+
+  return budsiesApi;
+}
