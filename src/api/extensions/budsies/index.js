33b84f06fe546ecdf064410d79845c4443f0090a
Diana <diana-lex@bk.ru>
2021-05-19 16:55:44 +0300

    Refs #24491: Budsies API extension - added proxy for getting upgrades list

--- src/api/extensions/budsies/index.js
+++ src/api/extensions/budsies/index.js
@@ -185,5 +185,37 @@ module.exports = ({ config, db }) => {
     });
   });
 
+  budsiesApi.get('/plushies/upgrades', (req, res) => {
+    const client = Magento1Client(multiStoreConfig(config.magento1.api, req));
+
+    client.addMethods('budsies', (restClient) => {
+      let module = {};
+
+      module.getShortCodes = function () {
+        const customerToken = getToken(req);
+
+        let url = `plushies/upgrades?token=${customerToken}`;
+
+        const productId = req.query.productId;
+
+        if (productId !== undefined) {
+          url += `&productId=${productId}`;
+        }
+
+        return restClient.get(url).then((data) => {
+          return getResponse(data);
+        });
+      }
+
+      return module;
+    });
+
+    client.budsies.getShortCodes().then((result) => {
+      apiStatus(res, result, 200);
+    }).catch(err => {
+      apiStatus(res, err, 500);
+    });
+  });
+
   return budsiesApi;
 }
