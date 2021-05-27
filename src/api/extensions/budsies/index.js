17bb27866201ee72b3001f4c6856c6d8a61116b9
fand0rin <andrei-vinogradov@yandex.ru>
2021-05-27 18:01:09 +0300

    Refs #24586: Budsies API extension - fix proxy methods names

--- src/api/extensions/budsies/index.js
+++ src/api/extensions/budsies/index.js
@@ -191,7 +191,7 @@ module.exports = ({ config, db }) => {
     client.addMethods('budsies', (restClient) => {
       let module = {};
 
-      module.getExtraPhotosAddons = function () {
+      module.getDogBreeds = function () {
         const customerToken = getToken(req);
 
         let url = `plushies/dogBreeds?token=${customerToken}`;
@@ -210,7 +210,7 @@ module.exports = ({ config, db }) => {
       return module;
     });
 
-    client.budsies.getExtraPhotosAddons().then((result) => {
+    client.budsies.getDogBreeds().then((result) => {
       apiStatus(res, result, 200);
     }).catch(err => {
       apiStatus(res, err, 500);
@@ -223,7 +223,7 @@ module.exports = ({ config, db }) => {
     client.addMethods('budsies', (restClient) => {
       let module = {};
 
-      module.getShortCodes = function () {
+      module.getPlushiesUpgrades = function () {
         const customerToken = getToken(req);
 
         let url = `plushies/upgrades?token=${customerToken}`;
@@ -242,7 +242,7 @@ module.exports = ({ config, db }) => {
       return module;
     });
 
-    client.budsies.getShortCodes().then((result) => {
+    client.budsies.getPlushiesUpgrades().then((result) => {
       apiStatus(res, result, 200);
     }).catch(err => {
       apiStatus(res, err, 500);
@@ -255,7 +255,7 @@ module.exports = ({ config, db }) => {
     client.addMethods('budsies', (restClient) => {
       let module = {};
 
-      module.getShortCodes = function () {
+      module.getPlushiesBodyParts = function () {
         const customerToken = getToken(req);
 
         let url = `plushies/bodyParts?token=${customerToken}`;
@@ -274,7 +274,7 @@ module.exports = ({ config, db }) => {
       return module;
     });
 
-    client.budsies.getShortCodes().then((result) => {
+    client.budsies.getPlushiesBodyParts().then((result) => {
       apiStatus(res, result, 200);
     }).catch(err => {
       apiStatus(res, err, 500);
