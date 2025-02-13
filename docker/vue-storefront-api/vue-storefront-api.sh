3c404027e34e5cdb357a5a51294bbf7400732159
Andrey Gorbunov <andrey@optimuspro.ru>
2025-02-13 14:05:02 +0100

    Refs #33956: Update environment configuration - add memory limits for Node.js instances and update ecosystem.json template

--- docker/vue-storefront-api/vue-storefront-api.sh
+++ docker/vue-storefront-api/vue-storefront-api.sh
@@ -2,11 +2,12 @@
 set -e
 
 envsubst < config/local.json.template > config/local.json
+envsubst < ecosystem.json.template > ecosystem.json
 
 yarn install || exit $?
 
 if [ "$VS_ENV" = 'dev' ]; then
-  yarn dev
+  yarn dev:inspect
 else
   yarn start
 fi
